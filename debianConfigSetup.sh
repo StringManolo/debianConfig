3dfc1a07e5c96d369fe29a915e890a84c0a0986e
StringManolo <56775178+StringManolo@users.noreply.github.com>
2023-07-15 20:11:39 +0200

    Update debianConfigSetup.sh

--- debianConfigSetup.sh
+++ debianConfigSetup.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-apt update && apt upgrade;
-apt install nodejs;
-apt install npm;
+yes | apt update && apt upgrade;
+yes | apt install nodejs;
+yes | apt install npm;
 npm install -g http-server;
-apt install tree;
-apt install vim;
-apt install curl;
-apt install git;
-apt install gh;
+yes | apt install tree;
+yes | apt install vim;
+yes | apt install curl;
+yes | apt install git;
+yes | apt install gh;
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
 curl 'https://raw.githubusercontent.com/StringManolo/debianConfig/d2bfbda769ca5859dff71e0475dd4d932e8d149a/configFiles/.vimrc' -o ~/.vimrc;
-apt install unzip;
+yes | apt install unzip;
 git clone https://github.com/StringManolo/debianConfig/;
 mv debianConfig/bin/aarch64/qj* ~/SMBSE/bin/;
 rm debianConfig/ -r;
-apt install p7zip-full;
+yes | apt install p7zip-full;
 curl 'https://raw.githubusercontent.com/StringManolo/SMBSE/main/.bashrc' -o ~/.basrc && source ~/.bashrc;
 
 
