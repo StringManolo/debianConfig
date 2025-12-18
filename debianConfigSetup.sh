09bc88a9f83a3c3518fa032e7abc4c4c49b925fb
StringManolo <manuelvarelacaldas@gmail.com>
2025-12-18 20:07:34 +0000

    Fix: quickjs download method

--- debianConfigSetup.sh
+++ debianConfigSetup.sh
@@ -14,9 +14,11 @@ apt install python3;
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
 curl 'https://raw.githubusercontent.com/StringManolo/debianConfig/refs/heads/main/configFiles/.vimrc' -o ~/.vimrc;
 yes | apt install unzip;
-git clone https://github.com/StringManolo/debianConfig/;
-mkdir ~/SMBSE/bin -p
-mv debianConfig/bin/aarch64/qj* ~/SMBSE/bin/;
+# APT HAS NOW QUICKJS BINS
+#git clone https://github.com/StringManolo/debianConfig/;
+#mkdir ~/SMBSE/bin -p
+#mv debianConfig/bin/aarch64/android/qj* ~/SMBSE/bin/;
+yes | apt install qjs;
 rm debianConfig/ -r;
 yes | apt install p7zip-full;
 
